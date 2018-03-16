b1e4ad609f0b40c4e03798a6cd17314ad95a287b
Maxime <leclet.maxime@busi-part.com>
2018-03-16 16:55:12 +0100

    TP5 : arrivee a Zone de Dessin

--- TP4/src/main.cpp
+++ TP4/src/main.cpp
@@ -2,9 +2,22 @@
 #include "figureGeometrique.hpp"
 #include "ligne.hpp"
 #include "polygoneRegulier.hpp"
+#include <vector>
 
 int main() {
 
-    
+    std::vector<FigureGeometrique *> figures;
+
+    figures.push_back(new Ligne(Couleur{0, 0, 0}, Point{1,2}, Point{2, 3}));
+    figures.push_back(new PolygoneRegulier(Couleur{0, 0, 0}, Point{1, 2}, 100, 6));
+    figures.push_back(new Ligne(Couleur{0, 0, 0}, Point{1,2}, Point{2, 3}));
+    figures.push_back(new PolygoneRegulier(Couleur{0, 0, 0}, Point{1, 2}, 100, 6));
+    figures.push_back(new Ligne(Couleur{0, 0, 0}, Point{1,2}, Point{2, 3}));
+
+    for(FigureGeometrique* figure: figures){
+       figure->afficher();
+     }
+
+    for(auto figure: figures) delete figure;
 
 }
---
+++ TP5/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "figureGeometrique.hpp"
+#include "ligne.hpp"
+#include "polygoneRegulier.hpp"
+#include "viewerFigures.hpp"
+#include <vector>
+
+int main(int argc, char ** argv) {
+
+    ViewerFigures viewerFigures(argc, argv);
+    viewerFigures.run();
+
+}
