d0fc90639201f7088c78d327bbb928a71f110a07
Maxime <leclet.maxime@busi-part.com>
2018-03-29 09:33:15 +0200

    "Fin" TP6 : arrivee apres les flux

--- TP5/src/main.cpp
+++ TP5/src/main.cpp
@@ -3,11 +3,15 @@
 #include "ligne.hpp"
 #include "polygoneRegulier.hpp"
 #include "viewerFigures.hpp"
+#include "zoneDessin.hpp"
 #include <vector>
 
 int main(int argc, char ** argv) {
 
     ViewerFigures viewerFigures(argc, argv);
     viewerFigures.run();
+
+    ZoneDessin zoneDessin;
+    
 
 }
---
+++ TP6/src/bibliotheque.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "bibliotheque.hpp"
+
+void Bibliotheque::afficher() const {
+
+    
+
+}
